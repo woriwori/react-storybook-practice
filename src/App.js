5ccade1f59933016010f3cd7055e275f0b198f41
Leena WH <wolee111@coupang.com>
2021-12-27 02:02:19 +0900

    change stories to use context

--- src/App.js
+++ src/App.js
@@ -1,14 +1,8 @@
 import React from 'react'
 import InboxScreen from './components/InboxScreen';
 import TaskContext from './context/TaskContext'
+import {defaultTasks} from './mock/tasks'
 
-// initial state
-const defaultTasks = [
-    { id: '1', title: 'Something', state: 'TASK_INBOX' },
-    { id: '2', title: 'Something more', state: 'TASK_INBOX' },
-    { id: '3', title: 'Something else', state: 'TASK_INBOX' },
-    { id: '4', title: 'Something again', state: 'TASK_INBOX' },
-];
 
 const App = () => (
     <TaskContext.Provider value={{tasks: defaultTasks}}>
---
+++ src/mock/tasks.js
@@ -0,0 +1,6 @@
+export const defaultTasks = [
+    { id: '1', title: 'Something', state: 'TASK_INBOX' },
+    { id: '2', title: 'Something more', state: 'TASK_INBOX' },
+    { id: '3', title: 'Something else', state: 'TASK_INBOX' },
+    { id: '4', title: 'Something again', state: 'TASK_INBOX' },
+];
